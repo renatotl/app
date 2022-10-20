9f8883e53118dd3ecd6bae7114c4a4e6f68a2f94
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-10-20 17:42:28 -0300

    feat: home componente

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,49 @@
+import * as S from "./style";
+import { ReactComponent as Search } from "assets/icons/search.svg";
+
+// o S da teg Home se refere  styled.section do Style
 const Home = () => {
-    return(<div>Componente Home</div>);
-  }
-  
-  export default Home;
+  return (
+    <S.Home>
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
+              <S.HomeHeaderDetailsDate>
+                Aqui ficará a data
+              </S.HomeHeaderDetailsDate>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <Search />
+              <input type="text" placeholder="Procure pelo sabor" />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeProductTitle>
+            <b>Pizzas</b>
+          </S.HomeProductTitle>
+          <S.HomeProductList>
+            <p>Lista de produtos aqui</p>
+          </S.HomeProductList>
+        </div>
+      </S.HomeContent>
+      <aside>
+        <p>Detalhes dos pedidos aqui</p>
+      </aside>
+    </S.Home>
+  );
+};
+
+export default Home;
+
+/*  import * as S from "./style";
+o s é um apelido do alias do arquivo style
+*/
+
+
+/* <aside>
+		<p>Detalhes dos pedidos aqui</p>
+	</aside> 
+   essa teg geralmente representa uma barra vertical ao lado */
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,1 +1,113 @@
-export const Home = {}
+import styled, { css } from "styled-components";
+
+
+export const Home = styled.section`
+	${({ theme }) => css`
+		background-color: ${theme.colors.baseBg1};
+		min-width: 100vw;
+		min-height: 100vh;
+		color: ${theme.colors.textColor};
+		display: flex;
+		justify-content: space-between;
+	`}
+`;
+
+//Voltando no style, vamos criar a const HomeContent estilizando a tag main:
+export const HomeContent = styled.main`
+	${() => css`
+		width: calc(100% - 450px);
+		padding: 20px;
+		overflow: auto;
+		height: 100vh;
+		box-sizing: border-box;
+	`}
+`;
+
+
+
+//vamos criar a const HomeHeaderDetails para estilizar uma tag div onde iremos inserir a nossa Logo:
+export const HomeHeaderDetails = styled.div`
+	${() => css`
+		display: flex;
+		justify-content: space-between;
+	`}
+`;
+
+
+// vamos criar as consts HomeHeaderDetailsLogo e HomeHeaderDetailsDate, estilizando a Logo e a data:
+export const HomeHeaderDetailsLogo = styled.h1`
+	${({ theme }) => css`
+		${theme.mixins.logoStyle};
+		text-transform: uppercase;
+		margin: 0;
+	`}
+`;
+
+export const HomeHeaderDetailsDate = styled.p`
+	${() => css`
+		text-transform: capitalize;
+	`}
+`;
+
+
+
+// vamos definir a const HomeHeaderDetailsSearch que será o componente estilizado para a barra de pesquisa:
+export const HomeHeaderDetailsSearch = styled.div`
+	${({ theme }) => css`
+		display: flex;
+		align-items: center;
+		gap: 10px;
+		${theme.mixins.input()};
+		padding-top: 0;
+		padding-bottom: 0;
+		padding-right: 0;
+		input {
+			background: transparent;
+			border: none;
+			height: 100%;
+			color: ${theme.colors.textColor};
+		}
+	`}
+`;
+
+
+//Voltando ao style.ts, vamos criar algumas consts para a estilização dos produtos na home:
+export const HomeProductTitle = styled.h2`
+	${({ theme }) => css`
+		margin: 0;
+		border-bottom: 1px solid ${theme.colors.baseLine};
+		${theme.mixins.bodyStyle()};
+		color: ${theme.colors.primaryColor};
+		padding-bottom: 10px;
+		b {
+			position: relative;
+			&::after {
+				content: "";
+				position: absolute;
+				left: 0;
+				bottom: -13px;
+				width: 100%;
+				height: 4px;
+				background-color: ${theme.colors.primaryColor};
+				border-radius: 5rem;
+			}
+		}
+	`}
+`;
+
+export const HomeProductList = styled.div`
+	${() => css`
+		margin-top: 25px;
+		padding-bottom: 25px;
+	`}
+`;
+
+export const HomeProductListGap = styled.div`
+	${({ theme }) => css`
+		width: 200px;
+	`}
+`;
+
+/* 
+Como vamos utilizar a biblioteca Styled Components para trabalhar com o CSS no nosso projeto, vale lembrar que a biblioteca torna possível que escrevamos, por exemplo, o CSS de uma div em um arquivo .js ou .ts, transformando então uma const em uma tag HTML já estilizada.
+*/
