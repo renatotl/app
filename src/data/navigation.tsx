b80da2a2cce1e1d26f5b6a48c1cc4ca16a7dbca3
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2023-01-28 20:49:35 -0300

    Update navigation.tsx
    imagem e apilidando-a de component

--- src/data/navigation.tsx
+++ src/data/navigation.tsx
@@ -1,13 +1,13 @@
 import { NavItem } from "components/Menu/types";
-import { ReactComponent as Home } from "assets/icons/home.svg";
-import { ReactComponent as Settings } from "assets/icons/settings.svg";
+import { ReactComponent as Home } from "assets/icons/home.svg";// importando uma imagem e apilidando-a de component
+import { ReactComponent as Settings } from "assets/icons/settings.svg";// importando uma imagem e apilidando-a de component
 import { RoutePath } from "types/routes";
 
 
-//os objet dentro do array 
+//os objet dentro do array. Obrigatoriamente o component navigationItems do typo NavItem precisa ter duas propriedades: Icon e Path
 export const navigationItems: NavItem[] = [
 	{
-		icon: <Home />,
+		icon: <Home />,// esse Home é este cara { ReactComponent as Home } 
 		path: RoutePath.HOME,// o seundo é o aminho que vai direionar 
 	},
 	{
