4cad61f24b0c02fc90015f36a6b5feaed54ba8f4
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2023-01-28 20:39:54 -0300

    Update types.ts
    caminho por onde o icon vai indicar

--- src/components/Menu/types.ts
+++ src/components/Menu/types.ts
@@ -5,5 +5,5 @@ import { RoutePath } from "types/routes";
 // define como um item de navegaçao deve ser
 export interface NavItem {
 	icon: JSX.Element;//icon no formato do elemento jsx
-	path: RoutePath;// caminho
+	path: RoutePath;// caminho por onde o icon vai indicar
 }
