b9fea8d327e8ef5cd5d84ecd0c0116c9efebae69
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2023-01-30 16:58:41 -0300

    Update index.tsx
    info

--- src/components/ButtonLarge/index.tsx
+++ src/components/ButtonLarge/index.tsx
@@ -4,7 +4,7 @@ import React, { ButtonHTMLAttributes } from "react";
 // buttonLarge é do tipo button 
 type ButtonType = ButtonHTMLAttributes<HTMLButtonElement>;
 
-
+// foi nescessário criar value aqui e passar como props para buton ligon onde é o nome do botão
 //E vamos criar um tipo ButtonLargeProps que recebe um objeto com o value e também ButtonType: para o value/ não esqucer de passar como props
 type ButtonLargeProps = {
     value: string;
